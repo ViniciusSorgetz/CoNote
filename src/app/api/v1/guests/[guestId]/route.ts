9c9e06dfcf12c404246bca9e965b1e367be8388b
Rexzinhoo <viniciussorgetzalves@gmail.com>
2025-04-21 09:07:27 -0300

    feat: Adjust `GET` in `guests/[guestId]` to return ALL  guest data

--- src/app/api/v1/guests/[guestId]/route.ts
+++ src/app/api/v1/guests/[guestId]/route.ts
@@ -1,6 +1,6 @@
 import errorHandler from "@/app/errors/errorHandler";
 import { NotFoundError } from "@/app/errors/errors";
-import { PrismaClient } from "@prisma/client";
+import { Folder, PrismaClient } from "@prisma/client";
 import { NextResponse } from "next/server";
 import { z } from "zod";
 
@@ -25,17 +25,7 @@ export async function GET(_req: Request, { params }: IParams) {
     const guest = await prisma.guest.findFirst({
       where: { id: guestId },
       include: {
-        folders: {
-          include: {
-            folders: {
-              include: {
-                folders: true,
-                notes: true,
-              },
-            },
-            notes: true,
-          },
-        },
+        folders: true,
       },
     });
 
@@ -43,8 +33,37 @@ export async function GET(_req: Request, { params }: IParams) {
       throw new NotFoundError({ message: "User not found" });
     }
 
+    const allFolders = await Promise.all(
+      guest.folders.map(async (folder: Folder) => await getSubFolders(folder)),
+    );
+
+    async function getSubFolders(folder: Folder) {
+      const subFolders = await prisma.folder.findMany({
+        where: { folderId: folder.id },
+      });
+      const notes = await prisma.note.findMany({
+        where: { folderId: folder.id },
+      });
+
+      if (subFolders.length > 0) {
+        const allSubFolders = await Promise.all(
+          subFolders.map(
+            async (subFolder: Folder) => await getSubFolders(subFolder),
+          ),
+        );
+        const updatedFolder = {
+          ...folder,
+          folders: allSubFolders,
+          notes: notes,
+        };
+        return updatedFolder;
+      }
+
+      return { ...folder, folders: [], notes: [] };
+    }
+
     return NextResponse.json(
-      { guest },
+      { guest: { ...guest, folders: allFolders } },
       {
         status: 200,
       },
